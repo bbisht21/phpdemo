d271262be41462febfc67a89f0da80917b503880
Balam Singh Bisht <bbisht@sapient.com>
2017-11-21 08:44:21 +0530

    Base framework

---
+++ src/main/java/com/git/phptravels/App.java
@@ -0,0 +1,13 @@
+package com.git.phptravels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
